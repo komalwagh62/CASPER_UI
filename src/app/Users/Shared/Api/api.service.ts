b53c5fe7fb89b929547f5484181e465f248584fb
VikasSahu1998 <sahuvikas1998@gmail.com>
2024-09-02 17:39:19 +0530

    OTP update

--- src/app/Users/Shared/Api/api.service.ts
+++ src/app/Users/Shared/Api/api.service.ts
@@ -13,7 +13,7 @@ import { map, Observable } from 'rxjs';
 export class ApiService {
   [x: string]: any;
   isAuthenticated: boolean = false;
-  public baseUrl: string = 'http://ec2-3-142-142-5.us-east-2.compute.amazonaws.com:3001/api';
+  public baseUrl: string = 'http://ec2-3-130-101-156.us-east-2.compute.amazonaws.com:3001/api';
   // public baseUrl: string = 'http://localhost:3001/api';
   public loginUserId: string = '';
   public userData!: User;
